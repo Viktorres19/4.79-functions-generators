f147054b569db9c3574b4c68c1691ed17d10d741
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-07-04 12:17:19 +0300

    finished the lesson

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/* // Simple usage
+function* generator() {
+    yield 'S';
+    yield 'c';
+    yield 'r';
+    yield 'i';
+    yield 'p';
+    yield 't';
+}
+
+const str = generator();
+
+console.log(str.next()); // { value: 'S', done: false }
+console.log(str.next()); // { value: 'c', done: false }
+console.log(str.next()); // { value: 'r', done: false }
+console.log(str.next()); // { value: 'i', done: false }
+console.log(str.next()); // { value: 'p', done: false }
+console.log(str.next()); // { value: 't', done: false }
+console.log(str.next()); // { value: undefined, done: true } */
+
+// More complex
+/* function* count(n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+const counter = count(7);
+
+console.log(counter.next().value); // 0
+console.log(counter.next().value); // 1
+console.log(counter.next().value); // 2 */
+
+
+//Other one
+function* count(n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+for (let k of count(7)) {
+    console.log(k); // 0, 1, 2, 3, 4, 5, 6
+}
